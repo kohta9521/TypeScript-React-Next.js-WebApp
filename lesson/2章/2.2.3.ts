997a5176b051c21ac7771ddaf46ce9dab8f08428
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-09-18 17:58:18 +0900

    オブジェクト型　first

---
+++ lesson/2章/2.2.3.ts
@@ -0,0 +1,5 @@
+const array: string[] = []
+array.push("Kohta")
+array.push(1) //配列の型と合わないためエラーが吐き出される
+
+
---
+++ lesson/2章/2.2.4.ts
@@ -0,0 +1,7 @@
+const user: {name: string; age: number} = {
+    name: 'Tokyo',
+    age: 36
+}
+
+console.log(user.name)
+console.log(user.age)
